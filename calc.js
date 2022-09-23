2bf5367e82feb166919518677ddaf0f44f0779f7
Kat M <kmunson13@gmail.com>
2022-09-23 08:33:08 -0500

    Cache DOM

---
+++ calc.js
@@ -0,0 +1,26 @@
+numberButton = document.querySelectorAll('.numberButton')
+addButton = document.querySelector('#add')
+subtractButton = document.querySelector('#subtract')
+multiplyButton = document.querySelector('#multiply')
+divideButton = document.querySelector('#divide')
+decimalButton = document.querySelector('#decimal')
+equalButton = document.querySelector('#equal')
+clearButton = document.querySelector('#clear')
+
+const add = function(a, b) {
+    return a + b};
+  
+const subtract = function(a, b) {
+ return a - b};
+
+const multiply = function(a, b) {
+    return a * b)
+  };
+
+const divide = function(a, b) {
+    return a / b
+}
+
+class Calculator {
+    constructor(currentNumber, previousNumber)
+}
