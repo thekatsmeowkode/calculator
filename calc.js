db1b9a76cf86933d73a872c8b3bf4339d820cb2b
Kat M <kmunson13@gmail.com>
2022-09-23 14:59:20 -0500

    Create working version

--- calc.js
+++ calc.js
@@ -17,10 +17,10 @@ buttons.addEventListener('click', (e) => {
     const keyReading = key.textContent
     const displayReading = screenBottom.textContent
     let previousKey = calcBody.dataset.previousKey
-
 if (!action) 
     {
-    if (displayReading === '0' || previousKey === 'operator') {screenBottom.textContent = keyReading}
+    calcBody.dataset.previousKey = 'number'
+    if (displayReading === '0' || previousKey === 'operator' || previousKey === 'equal') {screenBottom.textContent = keyReading}
     else {screenBottom.textContent = displayReading + keyReading}
     }
 if (action === 'add'
@@ -31,19 +31,40 @@ if (action === 'add'
     calcBody.dataset.firstOperand = displayReading;
     calcBody.dataset.operator = action}
 if (action === 'clear') {
-    console.log('clear')}
+    calcBody.dataset.previousKey = 'clear'
+    displayReading.textContent = 0
+    calcBody.dataset.firstOperand = ''
+    calcBody.dataset.secondOperand = ''
+    calcBody.dataset.valueStore = ''
+    calcBody.dataset.operator = ''}
 if (action === 'decimal') {
-    screenBottom.textContent = displayReading + '.'}
+    calcBody.dataset.previousKey = 'decimal';
+    if (!displayReading.includes('.')) {screenBottom.textContent = displayReading + '.'}
+    else if (previousKey === 'operator' || previousKey === 'equal') {screenBottom.textContent = '0'}}
 if (action === 'equal') {
     let secondOperand = displayReading;
     const firstOperand = calcBody.dataset.firstOperand;
     const operator = calcBody.dataset.operator
-    screenBottom.textContent = calculate(firstOperand, operator, secondOperand)}
-})}
+    if (firstOperand) {
+        if (previousKey === 'calculate') {
+            firstOperand = displayReading
+            secondOperand = calcBody.dataset.valueStore}
+        displayReading.textContent = calculate(firstOperand, operator, secondOperand)
+    }
+    calcBody.dataset.previousKey = 'operate'
+    calcBody.dataset.valueStore = secondOperand
+    if (firstOperand && operator && previousKey !== 'operator' && previousKey !=='equal') {
+        let calculateValue = calculate(firstOperand, operator, secondOperand)
+        screenBottom.textContent  = calculateValue
+        calcBody.dataset.firstOperand = calculateValue
+        calcBody.dataset.previousKey = 'equal'}
+    else {calcBody.dataset.firstOperand = displayReading}
+}})}
 
 function calculate(firstOperand, operator, secondOperand) {
-    if (operator === 'add') {return Number(firstOperand) + Number(secondOperand)}
-    if (operator === 'subtract') {return Number(firstOperand) - Number(secondOperand)}
-    if (operator === 'multiply') {return Number(firstOperand) * Number(secondOperand)}
-    if (operator === 'divide') {return Number(firstOperand) / Number(secondOperand)
-}
+    firstOperandFix = parseFloat(firstOperand)
+    secondOperandFix = parseFloat(secondOperand)
+    if (operator === 'add') {return firstOperandFix + secondOperand}
+    if (operator === 'subtract') {return firstOperandFix - secondOperand}
+    if (operator === 'multiply') {return firstOperandFix * secondOperand}
+    if (operator === 'divide') {return firstOperandFix / secondOperand}}
